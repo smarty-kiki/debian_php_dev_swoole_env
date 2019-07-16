99329fda1811f0fa7bb76165a2d8ff1e9571d78e
yaoyang <kiki@smarty.so>
2019-07-16 16:42:41 +0800

    调整为 swoole 模式

--- shell/start.sh
+++ shell/start.sh
@@ -6,13 +6,8 @@ then
     echo $TIMEZONE >/etc/timezone
 fi
 
-service nginx start
 service mysql start
-service redis-server start
-service php7.0-fpm start
-service beanstalkd start
 service supervisor start
-service mongodb start
 
 mysql -e "create database \`default\`"
 mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'password'"
